ca7996d7204dbe127b4b7fbea397a6509409a15d
Florent Pruvost <florent.pruvost@inria.fr>
2020-08-14 17:54:20 +0200

    control: Add the multi-runtime support (dlopen)

--- include/chameleon/runtime_struct.h
+++ include/chameleon/runtime_struct.h
@@ -45,10 +45,10 @@ BEGIN_C_DECLS
  * @brief Ids of the runtime supported by the RUNTIME API
  */
 typedef enum runtime_id_e {
-    RUNTIME_SCHED_QUARK,  /**< Quark runtime  */
-    RUNTIME_SCHED_PARSEC, /**< PaRSEC runtime */
-    RUNTIME_SCHED_STARPU, /**< StarPU runtime */
     RUNTIME_SCHED_OPENMP, /**< OpenMP runtime */
+    RUNTIME_SCHED_PARSEC, /**< PaRSEC runtime */
+    RUNTIME_SCHED_QUARK,  /**< Quark runtime  */
+    RUNTIME_SCHED_STARPU, /**< StarPU runtime */
 } RUNTIME_id_t;
 
 /**
