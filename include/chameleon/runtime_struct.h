ba769f1d2b01a0c4e84797da90a7c0ec85c6a698
LUCAS Gwenole <gwenole.lucas@inria.fr>
2020-11-18 14:37:24 +0100

    options/request: Add task and bubble_parent fileds to enable recursion and profiling

--- include/chameleon/runtime_struct.h
+++ include/chameleon/runtime_struct.h
@@ -66,15 +66,17 @@ typedef enum runtime_id_e {
  *
  */
 typedef struct runtime_request_s {
-    int   status;   /**< Return status registered by the tasks for the request   */
-    int   flush;    /**< Enable/disable automatic data flush within this request */
-    void *schedopt; /**< Specific runtime data pointer to handle the request     */
+    int   status;     /**< Return status registered by the tasks for the request */
+    int   flush;      /**< Enable/disable automatic data flush within this request */
+    void *parent;     /**< Specify the parent in recursive submission mode       */
+    void *dependency; /**< Specify the parent in recursive submission mode       */
+    void *schedopt;   /**< Specific runtime data pointer to handle the request   */
 } RUNTIME_request_t;
 
 /**
  *  @brief Runtime request initializer
  */
-#define RUNTIME_REQUEST_INITIALIZER { .status = 0, .flush = CHAMELEON_TRUE, .schedopt = NULL }
+#define RUNTIME_REQUEST_INITIALIZER { .status = 0, .flush = 1, .parent = NULL, .dependency = NULL, .schedopt = NULL }
 
 /**
  * @brief RUNTIME sequence structure
