f2b3c76fb26abafb834520f2d18088e7399f221b
Mathieu Faverge <mathieu.faverge@inria.fr>
2024-04-04 17:03:54 +0200

    Move the C++ protection

--- include/chameleon/runtime_struct.h
+++ include/chameleon/runtime_struct.h
@@ -25,8 +25,6 @@
 
 #include "chameleon/types.h"
 
-BEGIN_C_DECLS
-
 /**
  * Datatype for distributed version
  */
@@ -40,6 +38,8 @@ typedef uintptr_t MPI_Comm;
 #define MPI_COMM_WORLD 0
 #endif
 #endif
+
+BEGIN_C_DECLS
 
 /**
  * @brief Ids of the runtime supported by the RUNTIME API
