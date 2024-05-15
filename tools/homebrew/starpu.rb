a32d5df7ccef3975c8f4648169b0f985b8293d98
Florent Pruvost <florent.pruvost@inria.fr>
2024-05-15 12:54:44 +0200

    starpu.rb: update to 1.4.6

--- tools/homebrew/starpu.rb
+++ tools/homebrew/starpu.rb
@@ -13,8 +13,8 @@
 class Starpu < Formula
   desc "StarPU is a task programming library for hybrid architectures"
   homepage "https://starpu.gitlabpages.inria.fr/"
-  url "https://files.inria.fr/starpu/starpu-1.4.5/starpu-1.4.5.tar.gz"
-  sha256 "28f389b34df57a2a4e4743b40554b0f8d098ff2199e9eb8fbbe20aa377b64541"
+  url "https://files.inria.fr/starpu/starpu-1.4.6/starpu-1.4.6.tar.gz"
+  sha256 "579f38e150ab78c9ab299649d8649a043dbf49786e54acbf3dad90c160672cc7"
   license "GNU GPL v2.1"
 
   depends_on "autoconf" => :build
