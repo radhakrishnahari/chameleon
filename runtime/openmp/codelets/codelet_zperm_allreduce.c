dc30a4bbca335919792a5f382cf30e76b5f02bf9
Alycia Lisito <alycia.lisito@inria.fr>
2024-10-21 16:33:59 +0200

    zgetrf: add codelet_zperm_allreduce for quark, parsec and openmp

---
+++ runtime/openmp/codelets/codelet_zperm_allreduce.c
@@ -0,0 +1,93 @@
+/**
+ *
+ * @file openmp/codelet_zperm_allreduce.c
+ *
+ * @copyright 2012-2024 Bordeaux INP, CNRS (LaBRI UMR 5800), Inria,
+ *                      Univ. Bordeaux. All rights reserved.
+ *
+ ***
+ *
+ * @brief Chameleon openmp codelets to do the reduction
+ *
+ * @version 1.3.0
+ * @author Alycia Lisito
+ * @date 2024-06-11
+ * @precisions normal z -> c d s
+ *
+ */
+#include "chameleon_openmp.h"
+#include "chameleon/tasks_z.h"
+
+void
+INSERT_TASK_zperm_allreduce_send_A( const RUNTIME_option_t *options,
+                                    CHAM_desc_t            *A,
+                                    int                     Am,
+                                    int                     An,
+                                    int                     myrank,
+                                    int                     np,
+                                    int                    *proc_involved  )
+{
+    (void)options;
+    (void)A;
+    (void)Am;
+    (void)An;
+    (void)myrank;
+    (void)np;
+    (void)proc_involved;
+}
+
+void
+INSERT_TASK_zperm_allreduce_send_perm( const RUNTIME_option_t *options,
+                                       CHAM_ipiv_t            *ipiv,
+                                       int                     ipivk,
+                                       int                     myrank,
+                                       int                     np,
+                                       int                    *proc_involved  )
+{
+    (void)options;
+    (void)ipiv;
+    (void)ipivk;
+    (void)myrank;
+    (void)np;
+    (void)proc_involved;
+}
+
+void
+INSERT_TASK_zperm_allreduce_send_invp( const RUNTIME_option_t *options,
+                                       CHAM_ipiv_t            *ipiv,
+                                       int                     ipivk,
+                                       const CHAM_desc_t      *A,
+                                       int                     k,
+                                       int                     n )
+{
+    (void)options;
+    (void)ipiv;
+    (void)ipivk;
+    (void)A;
+    (void)k;
+    (void)n;
+}
+
+void
+INSERT_TASK_zperm_allreduce( const RUNTIME_option_t *options,
+                             const CHAM_desc_t      *A,
+                             CHAM_ipiv_t            *ipiv,
+                             int                     ipivk,
+                             int                     k,
+                             int                     n,
+                             CHAM_desc_t            *U,
+                             int                     Um,
+                             int                     Un,
+                             void                   *ws )
+{
+    (void)options;
+    (void)A;
+    (void)ipiv;
+    (void)ipivk;
+    (void)k;
+    (void)n;
+    (void)U;
+    (void)Um;
+    (void)Un;
+    (void)ws;
+}
