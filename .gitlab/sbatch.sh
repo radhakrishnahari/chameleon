dcb38b66c2d6de1d84675dc1069d990fb05ee9b6
Florent Pruvost <florent.pruvost@inria.fr>
2024-12-18 08:41:11 +0100

    ci: add test_starpu_cuda

---
+++ .gitlab/sbatch.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# sbatch.sh : submit slurm jobs and wait for completion before exiting
+set -x
+
+if [ $# -gt 0 ]
+then
+    JOB_NAME=$1
+fi
+JOB_NAME=${JOB_NAME:-chameleon}
+
+# to get kernels execution on both cpus and gpus
+export STARPU_SCHED=random
+
+# execution commands
+sbatch --wait \
+       --job-name="$JOB_NAME" \
+       --output="$JOB_NAME.out" \
+       --nodes=1 \
+       --exclusive --ntasks-per-node=1 --threads-per-core=1 \
+       --constraint="$SLURM_CONSTRAINTS" \
+       --time=01:00:00 \
+       $(dirname "$0")/test.sh
+# get the error code from the last command: sbatch --wait ...
+err=$?
+
+cat $JOB_NAME.out
+
+# exit with error code from the guix command
+exit $err
