872705790e52a236e0946c014cc329a38f784235
Mathieu Faverge <mathieu.faverge@inria.fr>
2025-02-17 14:05:29 +0100

    starpu/codelet: Add new task submit to codelet_zherk.c

--- runtime/starpu/codelets/codelet_zherk.c
+++ runtime/starpu/codelets/codelet_zherk.c
@@ -29,12 +29,12 @@
 #include "runtime_codelet_z.h"
 
 struct cl_zherk_args_s {
-    cham_uplo_t uplo;
+    cham_uplo_t  uplo;
     cham_trans_t trans;
-    int n;
-    int k;
-    double alpha;
-    double beta;
+    int          n;
+    int          k;
+    double       alpha;
+    double       beta;
 };
 
 #if !defined(CHAMELEON_SIMULATION)
@@ -109,6 +109,7 @@ CODELETS_GPU( zherk, cl_zherk_cpu_func, cl_zherk_hip_func, STARPU_HIP_ASYNC )
 CODELETS( zherk, cl_zherk_cpu_func, cl_zherk_cuda_func, STARPU_CUDA_ASYNC )
 #endif
 
+#if defined(CHAMELEON_STARPU_USE_INSERT)
 void INSERT_TASK_zherk( const RUNTIME_option_t *options,
                         cham_uplo_t uplo, cham_trans_t trans,
                         int n, int k, int nb,
@@ -121,11 +122,11 @@ void INSERT_TASK_zherk( const RUNTIME_option_t *options,
         return;
     }
 
-    struct cl_zherk_args_s *clargs = NULL;
     void (*callback)(void*);
-    int                      accessC;
-    int                      exec = 0;
-    const char              *cl_name = "zherk";
+    struct cl_zherk_args_s *clargs  = NULL;
+    int                     exec    = 0;
+    const char             *cl_name = "zherk";
+    int                     accessC;
 
     /* Handle cache */
     CHAMELEON_BEGIN_ACCESS_DECLARATION;
@@ -173,3 +174,83 @@ void INSERT_TASK_zherk( const RUNTIME_option_t *options,
 
     (void)nb;
 }
+
+#else
+
+void INSERT_TASK_zherk( const RUNTIME_option_t *options,
+                        cham_uplo_t uplo, cham_trans_t trans,
+                        int n, int k, int nb,
+                        double alpha, const CHAM_desc_t *A, int Am, int An,
+                        double beta,  const CHAM_desc_t *C, int Cm, int Cn )
+{
+    if ( alpha == 0. ) {
+        INSERT_TASK_zlascal( options, uplo, n, n, nb,
+                             beta, C, Cm, Cn );
+        return;
+    }
+
+    INSERT_TASK_COMMON_PARAMETERS( zherk, 2 );
+    int accessC;
+
+    /* Reduce the C access if needed */
+    accessC = ( beta == (double)0. ) ? STARPU_W : STARPU_RW;
+
+    /*
+     * Set the data handles and initialize exchanges if needed
+     */
+    starpu_cham_exchange_init_params( options, &params, C->get_rankof( C, Cm, Cn ) );
+    starpu_cham_exchange_data_before_execution( options, params, &nbdata, descrs, A, Am, An, STARPU_R );
+    starpu_cham_exchange_data_before_execution( options, params, &nbdata, descrs, C, Cm, Cn, accessC  );
+
+    /*
+     * Not involved, let's return
+     */
+    if ( nbdata == 0 ) {
+        return;
+    }
+
+    if ( params.do_execute )
+    {
+        int ret;
+        struct starpu_task *task = starpu_task_create();
+        task->cl = cl;
+
+        /* Set codelet parameters */
+        clargs = malloc( sizeof( struct cl_zherk_args_s ) );
+        clargs->uplo  = uplo;
+        clargs->trans = trans;
+        clargs->n     = n;
+        clargs->k     = k;
+        clargs->alpha = alpha;
+        clargs->beta  = beta;
+
+        task->cl_arg      = clargs;
+        task->cl_arg_size = sizeof( struct cl_zherk_args_s );
+        task->cl_arg_free = 1;
+
+        /* Set common parameters */
+        starpu_cham_task_set_options( options, task, nbdata, descrs, cl_zherk_callback );
+
+        /* Flops */
+        task->flops = flops_zherk( k, n );
+
+        /* Refine name */
+        task->name = chameleon_codelet_name( cl_name, 2,
+                                             A->get_blktile( A, Am, An ),
+                                             C->get_blktile( C, Cm, Cn ) );
+
+        ret = starpu_task_submit( task );
+        if ( ret == -ENODEV ) {
+            task->destroy = 0;
+            starpu_task_destroy( task );
+            chameleon_error( "INSERT_TASK_zherk", "Failed to submit the task to StarPU" );
+            return;
+        }
+    }
+
+    starpu_cham_task_exchange_data_after_execution( options, params, nbdata, descrs );
+
+    (void)nb;
+}
+
+#endif
