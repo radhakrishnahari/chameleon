ce2bd98d4458bbaf1642afd73c9252faaaf44cad
Matteo Marcos <matteo.marcos@inria.fr>
2025-04-10 15:05:52 +0200

    codelet: Add codelet for batched column permutation

--- runtime/openmp/codelets/codelet_zlaswpc.c
+++ runtime/openmp/codelets/codelet_zlaswpc.c
@@ -2,7 +2,7 @@
  *
  * @file openmp/codelet_zlaswpc.c
  *
- * @copyright 2012-2025 Bordeaux INP, CNRS (LaBRI UMR 5800), Inria,
+ * @copyright 2025-2025 Bordeaux INP, CNRS (LaBRI UMR 5800), Inria,
  *                      Univ. Bordeaux. All rights reserved.
  *
  ***
